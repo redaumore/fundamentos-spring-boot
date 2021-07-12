d9c463f33e3504804b3cc282f054ff2ca0c2bb5f
rolando <redaumore@gmail.com>
2021-07-12 19:42:39 -0300

    first commit

---
+++ src/test/java/com/rolando/training/SpringBootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolando.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
